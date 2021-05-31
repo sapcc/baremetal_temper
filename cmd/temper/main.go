7852db1318a92c63761c9792213484bad15b0627
Hipfel, Stefan <stefan.hipfel@sap.com>
2021-05-31 10:27:52 +0200

    improves error handling; adds temper server main

--- cmd/temper/main.go
+++ cmd/temper/main.go
@@ -16,6 +16,85 @@
 
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/evalphobia/logrus_sentry"
+	"github.com/sapcc/baremetal_temper/cmd"
+	"github.com/sapcc/baremetal_temper/pkg/config"
+	"github.com/sapcc/baremetal_temper/pkg/server"
+	"github.com/sapcc/baremetal_temper/pkg/temper"
+	log "github.com/sirupsen/logrus"
+)
+
+var opts config.Options
+var wait time.Duration
+
+func main() {
+	var cfg config.Config
+	cmd.InitConfig()
+	cmd.ReadInConfig(&cfg)
+	ctxLogger := log.WithFields(log.Fields{
+		"temper": "",
+	})
+	t := temper.New(opts.Workers)
+	s := server.New(cfg, ctxLogger, t)
+	srv := &http.Server{
+		Addr: "0.0.0.0:8080",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      s.Router, // Pass our instance of gorilla/mux in.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+
+}
+
+func init() {
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.IntVar(&opts.Workers, "number-Workers", 10, "set the number of temper workers to handle tempering of nodes simultaneously")
+	flag.Parse()
+
+	// default log level
+	opts.LogLevelValue = config.LogLevelValue{LogLevel: log.DebugLevel}
+	log.SetLevel(opts.LogLevelValue.LogLevel)
+
+	dsn := os.Getenv("SENTRY_DSN")
+	if dsn != "" {
+		hook, err := logrus_sentry.NewSentryHook(dsn, []log.Level{
+			log.PanicLevel,
+			log.FatalLevel,
+			log.ErrorLevel,
+		})
+		if err == nil {
+			log.Info("adding sentry hook")
+			log.AddHook(hook)
+		}
+	}
 }
