d9482cbe6522176ca7b70658b690893d1be9da7e
Stefan Hipfel <stefan.hipfel@sap.com>
2024-01-30 14:52:29 +0100

    addHostToAggregate: check if header value is empty

--- pkg/node/openstack.go
+++ pkg/node/openstack.go
@@ -281,7 +281,7 @@ func (n *Node) addHostToAggregate(host, az string) (err error) {
 	}
 	if !foundHost {
 		r := aggregates.AddHost(cl, aggregate.ID, aggregates.AddHostOpts{Host: host})
-		if r.Header.Values("Status")[0] == "409" {
+		if len(r.Header.Values("Status")) > 0 && r.Header.Values("Status")[0] == "409" {
 			return
 		}
 		return r.Err
